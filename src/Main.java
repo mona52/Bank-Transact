b49b41ccd7d1fc5b492c090a3ba698ff062c34ab
mona52 <mona52@list.ru>
2021-01-13 23:25:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account_balance_current = 2_000_000_000;
+        int sum_of_transaction = 500_000_000;
+        int account_balance_end = account_balance_current + sum_of_transaction;
+        System.out.println(account_balance_end);
+    }
+}
